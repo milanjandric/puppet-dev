133add34f08581ebcdc68f0f1f99ca7c889d8445
Milan Jandric <milan.jandric@saturized.com>
2012-12-15 23:56:07 +0100

    added ssh keys for root

--- modules/root/manifests/init.pp
+++ modules/root/manifests/init.pp
@@ -19,4 +19,53 @@ class root
       source  => "puppet:///modules/zsh/etc/skel/.zshrc",
       require => Package["zsh"],
   }
+
+  file
+  {
+    "/root/.ssh":
+      ensure => directory,
+      mode   => 700,
+      owner  => root,
+      group  => root,
+  }
+
+  file
+  {
+    "/root/.ssh/authorized_keys":
+      ensure => present,
+      mode   => 600,
+      owner  => root,
+      group  => root,
+      source => "puppet:///modules/root/root/.ssh/authorized_keys",
+  }
+
+  file
+  {
+    "/root/.ssh/id_dsa":
+      ensure => present,
+      mode   => 600,
+      owner  => root,
+      group  => root,
+      source => "puppet:///modules/root/root/.ssh/id_dsa",
+  }
+
+  file
+  {
+    "/root/.ssh/id_dsa.pub":
+      ensure => present,
+      mode   => 600,
+      owner  => root,
+      group  => root,
+      source => "puppet:///modules/root/root/.ssh/id_dsa.pub",
+  }
+
+  file
+  {
+    "/root/.ssh/config":
+      ensure => present,
+      mode   => 600,
+      owner  => root,
+      group  => root,
+      source => "puppet:///modules/root/root/.ssh/config",
+  }
 }
